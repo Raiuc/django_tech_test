ebd8ca8b3422d32ffdb063ffa89578c3bd86f2e7
Cuitláhuac Acuña <poligono@gmail.com>
2018-08-27 20:27:19 -0500

    Model admin config for models
    Admin configuration for Django Administration.

--- apps/lines/admin.py
+++ apps/lines/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import LineModel, RouteModel
 
-# Register your models here.
+# Model visualization/editting in Django admin
+class LineModelAdmin(admin.ModelAdmin):
+    fields = ['id', 'name', 'color']
+
+class RouteModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'line', 'get_stations', 'direction', 'is_active']
+    list_filter = ['id', 'line', 'direction', 'is_active']
+    list_per_page = 500
+
+    def get_stations(self, obj):
+        return "\n".join([p.stations for p in obj.stations.all()])
+
+
+admin.site.register(LineModel, LineModelAdmin)
+admin.site.register(RouteModel, RouteModelAdmin)
--- apps/stations/admin.py
+++ apps/stations/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models.locations import LocationModel
+from .models.stations import StationModel
 
-# Register your models here.
+# Model visualization/editting in Django admin
+class LocationModelAdmin(admin.ModelAdmin):
+    list_filter = ['id', 'name', 'latitude', 'longitude']
+    list_per_page = 500
+
+
+class StationModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'location', 'order', 'is_active']
+    list_per_page = 500
+
+admin.site.register(LocationModel, LocationModelAdmin)
+admin.site.register(StationModel, StationModelAdmin)
